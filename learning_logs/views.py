b6849744453a7a7dbd682edccad61b8d15f54efb
Kamil <kamil.dominko@gmail.com>
2024-02-27 23:55:23 +0100

    Dodanie własności danych użytkownikom.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect, Http404
 
 
 # Create your views here.
@@ -10,21 +12,28 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Wyświetlenie wszystkich tematów."""
-    topics = Topic.objects.order_by('date_added')
+    # topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Wyświetla pojedynczy temat i wszystkie powiązane z nim wpisy."""
     topic = Topic.objects.get(id=topic_id)
+    # Upewniamy się, że temat należy do bieżącego użytkownika.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Dodaj nowy temat"""
     if request.method != 'POST':
@@ -34,7 +43,9 @@ def new_topic(request):
         # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Wyświetlenie pustego formularza.
@@ -42,6 +53,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Dodawanie nowego wpisu dla określonego tematu."""
     topic = Topic.objects.get(id=topic_id)
@@ -63,10 +75,13 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edycja istniejącego wpisu."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Żądanie początkowe, wypełnienie formularza aktualną treścią wpisu.
